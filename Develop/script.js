18b21537837aaf0f94c8bf2a033d17c4bc0186b3
ebo16@vt.edu <ebo16@vt.edu>
2021-09-19 10:29:49 -0400

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//ask length of password 8-128
+//ask to include lowercase, uppercase, numeric, and/or special characters
+//oen character type must be selected
+//password generated and displayed
